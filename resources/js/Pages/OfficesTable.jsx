7a273b24ca14ec62e1c66311afee1bef521423ba
DaviMeda <davileo.meda@gmail.com>
2024-05-14 13:11:42 +0200

    OfficeTable updated

--- resources/js/Pages/OfficesTable.jsx
+++ resources/js/Pages/OfficesTable.jsx
@@ -35,7 +35,7 @@ export default function OfficesTable()
             <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-1 lg:grid-cols-2">
       	    	{offices.map((office) => (
       	    	  <li key={office.id} className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
-      	    	    <Link to="/offices/${office.id}" className="flex w-full items-center justify-between space-x-6 p-6">
+      	    	    <Link to={`/offices/${office.id}`} className="flex w-full items-center justify-between space-x-6 p-6">
       	    	      <div className="flex-1 truncate">
       	    	        <div className="flex items-center space-x-3">
       	    	          <h3 className="truncate text-sm font-medium text-gray-900">{office.name} & {office.id}</h3>
