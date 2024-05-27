1c2e7cb168ae8ad73eeb7e9486443f82c47514cd
Rony Cesana <9611986+rony76@users.noreply.github.com>
2024-05-27 15:25:31 +0200

    Utilizza un router client side che usi solo gli hash

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
+Route::get('/app', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
@@ -25,10 +25,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    
-
-    
 });
 
 require __DIR__.'/auth.php';
