46decb63207762b43900d0120763abc793a7cd94
Rony Cesana <rony.cesana@thoughtworks.com>
2024-05-27 16:14:07 +0200

    Clean up grafico per le disponibilità tavoli

--- resources/js/Pages/DeskAvailability.jsx
+++ resources/js/Pages/DeskAvailability.jsx
@@ -25,21 +25,26 @@ export function DeskAvailability({ userId }) {
     };
 
     return (
-        <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-1 lg:grid-cols-2">
-            {desks.map((desk) => (
-                <li key={desk.id} className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
-                    <div className="flex-1 truncate">
-                        <div className="space-x-3 text-center">
-                            <h2 className="truncate text-gray-900 py-5 text-center mx-auto ">Desk {desk.name}</h2>
-                            <div className="my-3 flex justify-evenly">
-                                <DeskReservation currentUser={userId} deskState={desk.am} amOrPm='morning' />
-                                <DeskReservation currentUser={userId} deskState={desk.pm} amOrPm='afternoon' />
+        <>
+            <header className="bg-white shadow p-4 text-center">
+                <h1>Desk Availability</h1>
+            </header>
+            <section>
+                <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-1 lg:grid-cols-2">
+                    {desks.map((desk) => (
+                        <li key={desk.id} className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
+                            <div className="flex-1 truncate">
+                                <h2 className="truncate text-gray-900 py-5 text-center mx-auto ">Desk {desk.name}</h2>
+                                <div className="my-3 flex justify-evenly">
+                                    <DeskReservation currentUser={userId} deskState={desk.am} amOrPm='morning' />
+                                    <DeskReservation currentUser={userId} deskState={desk.pm} amOrPm='afternoon' />
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                </li>
-            ))}
-        </ul>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </>
     );
 
 }
