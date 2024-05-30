80fa4d6effac6ab29b75044b1926649e2ddd5f9a
DaviMeda <davileo.meda@gmail.com>
2024-05-30 19:22:51 +0200

    Evita di creare una prenotazione temporanea

--- resources/js/Services/desk-service.js
+++ resources/js/Services/desk-service.js
@@ -11,3 +11,28 @@ export async function getOffices() {
     const data = await response.json();
     return data;
 }
+
+export async function createReservation(userId, formattedDate) {
+    try {
+      const response = await fetch('http://localhost:8000/api/postReservation', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user_id: userId,
+          desk_id: null,
+          reservation_day: formattedDate,
+          morning_busy: false,
+          afternoon_busy: false,
+        }),
+      });
+      if (response.ok) {
+        console.log('Prenotazione creata con successo!');
+      } else {
+        console.error('Errore durante la creazione della prenotazione:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Errore durante la creazione della prenotazione:', error);
+    }
+  };
