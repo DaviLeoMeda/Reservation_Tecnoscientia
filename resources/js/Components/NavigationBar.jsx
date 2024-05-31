430f66cfcc6765b93a6d325508204dfdf8da18f1
DaviMeda <davileo.meda@gmail.com>
2024-05-31 15:58:14 +0200

    Rimuovi il link alla dashboard nel sottomenù

--- resources/js/Components/NavigationBar.jsx
+++ resources/js/Components/NavigationBar.jsx
@@ -1,19 +1,12 @@
 import { Link } from 'react-router-dom';
 
 export default function NavigationBar() {
-    return <nav className="py-6 bg-white shadow flex justify-around">
-        <div>
-            <Link className='px-10' to="/">
-                Dashboard
-            </Link>
-        </div>
-        <div className="flex items-center">
-            <Link className='px-10' to="/offices">
-                Desks per day
-            </Link>
-            <Link className='px-10' to="/my-reservations">
-                My Reservations
-            </Link>
-        </div>
+    return <nav className="py-6 bg-white shadow flex justify-end">
+        <Link className='px-10' to="/offices">
+            Desks per day
+        </Link>
+        <Link className='px-10' to="/my-reservations">
+            My Reservations
+        </Link>
     </nav>
 }
